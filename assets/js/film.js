b49e9f2424791d07ffecf839984aa4ca1bad2bde
William27310 <william.27@free.fr>
2025-07-11 11:17:08 +0200

    movie

---
+++ assets/js/film.js
@@ -0,0 +1,67 @@
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Y2NmMTY2YjU4Njk1ZTBhMGM1OGRjMzQ0YTdmN2U1NCIsIm5iZiI6MTc1MTk3Mjc4NS43OTQsInN1YiI6IjY4NmNmYmIxMjE1MGMxZGNkZTZlODhlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Upg7Opwv3uxOg3ODDA9qqFP0ZFG9sxttAw-kO7I5hdI'
+    }
+};
+
+const parsedUrl = new URL(window.location.href);
+
+let id = parsedUrl.searchParams.get("id")
+
+fetch(`https://api.themoviedb.org/3/movie/${id}?language=fr-fr`, options)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        console.log(data.original_title)
+        console.log(data.poster_path)
+
+
+
+        document.getElementById("details").innerHTML = `
+            <div class="d-flex justify-content-start border" id="details">
+                <img class="ms-5" src="https://image.tmdb.org/t/p/w500${data.poster_path}"
+                alt="">
+                <ul>
+                    <li type="none">
+                        <p class="ms-2 mt-5">${data.original_title}</p>
+                    </li>
+                    <li type="none">
+                        <p class="ms-2 mt-5">${data.vote_average}</p>
+                    </li>
+                    <li type="none">
+                        <p class="ms-2 mt-5">${data.overview}</p>
+                    </li>
+                    <li type="none">
+                        <p class="ms-2 mt-5">${data.release_date}</p>
+                    </li>
+                </ul>
+            </div>
+        `
+    })
+    .catch(err => console.error(err));
+
+let total_acteur = 15
+
+fetch(`https://api.themoviedb.org/3/movie/${id}/credits?language=fr-fr`, options)
+    .then(response => response.json())
+    .then(acteur => {
+        console.log(acteur)
+
+        document.getElementById("casting").innerHTML = ""
+        for (let i = 0; i < total_acteur; i++) {
+            document.getElementById("casting").innerHTML += 
+            `
+            <div class="col-2 card">
+                <img src="https://image.tmdb.org/t/p/w500${acteur.cast[i].profile_path}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text"> ${acteur.cast[i].name} </p>
+                    <p class="card-text"> ${acteur.cast[i].character}</p>
+                </div>
+            </div>
+        `
+
+        }
+    })
+    .catch(err => console.error(err));
---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Y2NmMTY2YjU4Njk1ZTBhMGM1OGRjMzQ0YTdmN2U1NCIsIm5iZiI6MTc1MTk3Mjc4NS43OTQsInN1YiI6IjY4NmNmYmIxMjE1MGMxZGNkZTZlODhlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Upg7Opwv3uxOg3ODDA9qqFP0ZFG9sxttAw-kO7I5hdI'
+    }
+};
+
+let total_film = 5
+
+fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+        document.getElementById("receptacle").innerHTML = ""
+        for (let i = 0; i < total_film; i++) {
+            document.getElementById("receptacle").innerHTML += `
+            <div class="col-6 col-sm-3">
+            <div class="cards h-100">
+                    <img class="img-fluid" src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}"
+                    alt="...">
+                <div class="card-body">
+                    <a class="text-decoration-none" href="film.html?id=${data.results[i].id}"><h5 class="mt-2">${data.results[i].title}</h5></a>
+                    <p>${data.results[i].release_date}</p>
+                    <p>${data.results[i].vote_average.toFixed(1)}</p>
+                </div>
+        </div> 
+        </div>           
+            `
+        }
+    })
+    .catch(err => console.error(err));
